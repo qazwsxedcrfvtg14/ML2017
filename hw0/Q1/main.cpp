5125d23bcdb1fbfc087dc0baeab5d499e729964f
qazwsxedcrfvtg14 <qazwsxedcrfvtg14@gmail.com>
2017-02-23 16:54:26 +0800

    hw0-fix

--- hw0/Q1/main.cpp
+++ hw0/Q1/main.cpp
@@ -5,172 +5,6 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
-template<unsigned int N,typename T=int>
-struct cub{
-    T a[N][N];
-    int n,m;
-    cub(int _n,int _m,T k=0){
-        n=_n;
-        m=_m;
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                a[i][j]=0;
-        if(k)
-            for(int i=0;i<n;i++)
-                a[i][i]=k;
-        }
-    cub(T k=0){
-        n=N;
-        m=N;
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                a[i][j]=0;
-        if(k)
-            for(int i=0;i<n;i++)
-                a[i][i]=k;
-        }
-    void rebuild(int _n=0,int _m=0,T k=0){
-        n=_n;
-        m=_m;
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                a[i][j]=0;
-        if(k)
-            for(int i=0;i<n;i++)
-                a[i][i]=k;
-        }
-    inline T* operator [](int x){
-        return a[x];
-        }
-    cub operator +(cub b){
-        if(n!=b.n||m!=b.m)throw("+ error");
-        cub c(n,m);
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                c[i][j]=a[i][j]+b[i][j];
-        return c;
-        }
-    cub operator -(cub b){
-        if(n!=b.n||m!=b.m)throw("- error");
-        cub c(n,m);
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                c[i][j]=a[i][j]-b[i][j];
-        return c;
-        }
-    cub operator *(cub b){
-        if(m!=b.n)throw("* error");
-        cub c(n,b.m);
-        for(int i=0;i<n;i++)
-            for(int j=0;j<b.m;j++)
-                for(int k=0;k<m;k++)
-                    c[i][j]+=a[i][k]*b[k][j];
-        return c;
-        }
-    cub operator *(T b){
-        cub c(n,m);
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                c[i][j]=a[i][j]*b;
-        return c;
-        }
-    cub del(int x,int y){
-        if(x<0||y<0||x>=n||y>=m)throw("del error");
-        cub c(n-1,m-1);
-        for(int i=0,ci=0;i<n;i++,ci++){
-            if(i==x){ci--;continue;}
-            for(int j=0,cj=0;j<m;j++,cj++){
-                if(j==y){cj--;continue;}
-                c[ci][cj]=a[i][j];
-                }
-            }
-        return c;
-        }
-    T det(){
-        if(n!=m)throw("det error");
-        if(n==1)return a[0][0];
-        if(n==2)return a[0][0]*a[1][1]-a[0][1]*a[1][0];
-        T ret=0;
-        int t=1;
-        for(int i=0;i<n;i++){
-            ret+=t*a[0][i]*del(0,i).det();
-            t=-t;
-            }
-        return ret;
-        }
-    cub gus(){
-        if(n>m)throw("gus error");
-        cub mtx=*this;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++)
-                if(mtx[j][i].p){
-                    for(int k=0;k<m;k++)
-                        swap(mtx[i][k],mtx[j][k]);
-                    break;
-                    }
-            if(!mtx[i][i].p)continue;
-            for(int k=n;k>=i;k--)
-                mtx[i][k]=mtx[i][k]/mtx[i][i];
-            for(int j=i+1;j<n;j++)
-                for(int k=m-1;k>=i;k--)
-                    mtx[j][k]=mtx[j][k]-mtx[i][k]*mtx[j][i];
-            }
-        for(int i=n-1;i>=0;i--){
-            for(int j=i-1;j>=0;j--){
-                for(int k=n;k<m;k++)
-                    mtx[j][k]=mtx[j][k]-mtx[i][k]*mtx[j][i];
-                mtx[j][i]=0;
-                }
-            }
-        return mtx;
-        }
-    cub Tr(){
-        cub c(m,n);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                c[j][i]=a[i][j];
-                }
-            }
-        return c;
-        }
-    cub operator -(){
-        if(!det())throw("~ error");
-        cub d(n,n);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                d[i][j]=del(j,i).det()*(((i+j)&1)?-1:1);
-                }
-            }
-        return d;
-        }
-    cub operator ~(){
-        return (-*this)*(1.0/det());
-        }
-    cub operator /(cub b){
-        return (*this*(-b));//*b.det();
-        }
-    cub operator %(cub b){
-        return ((-*this)*b);//*det();
-        }
-    cub operator ^(long long int b){
-        cub ret(n,m,1),now=*this;
-        while(b){
-            if(b&1)ret=ret*now;
-            now=now*now;
-            b>>=1;
-            }
-        return ret;
-        }
-    void print(){
-        printf("[ ");
-        for(int i=0;i<n;i++){
-            if(i)printf("; ");
-            for(int j=0;j<m;j++)
-                printf("%d ",a[i][j]);
-            }
-        printf("]\n");
-        }
-    };
 vector<string>split(string s,string cut){
     vector<string>ve;
     if(s=="")return ve;
@@ -201,7 +35,7 @@ int StrToInt(string x){
     str>>s;
     return s;
     }
-cub<50> a(1,50),b(50,10),c(1,10);
+vector<vector<int>>a,b,c;
 int main(int argc,char** argv){
     string fa,fb;
     if(argv[1]) fa=argv[1];
@@ -217,11 +51,11 @@ int main(int argc,char** argv){
         fprintf(stderr,"%s not found!\n",fa.c_str());
         exit(0);
         }
-    for(int i=0;i<1;i++){
-        getline(fin,s);
+    for(int i=0;getline(fin,s)&&s!="";i++){
         auto v=split(s,",");
-        for(int j=0;j<50;j++)
-            a[i][j]=StrToInt(v[j]);
+        a.push_back(vector<int>());
+        for(auto &x:v)
+            a.back().push_back(StrToInt(x));
         }
     fin.close();
     fin.open(fb,ios::in);
@@ -229,17 +63,31 @@ int main(int argc,char** argv){
         fprintf(stderr,"%s not found!\n",fb.c_str());
         exit(0);
         }
-    for(int i=0;i<50;i++){
-        getline(fin,s);
+    for(int i=0;getline(fin,s)&&s!="";i++){
         auto v=split(s,",");
-        for(int j=0;j<10;j++)
-            b[i][j]=StrToInt(v[j]);
+        b.push_back(vector<int>());
+        for(auto &x:v)
+            b.back().push_back(StrToInt(x));
         }
     fin.close();
-    c=a*b;
-    sort(c[0],c[0]+10);
-    for(int i=0;i<1;i++)
-        for(int j=0;j<10;j++)
-            printf("%d\n",c[i][j]);
+    if(a[0].size()!=b.size()){
+        fprintf(stderr,"Matrix is not multipliable!\n");
+        exit(0);
+        }
+    vector<int>ans;
+    for(unsigned int i=0;i<a.size();i++){
+        //c.push_back(vector<int>());
+        for(unsigned int j=0;j<b[0].size();j++){
+            //c[i].push_back(0);
+            ans.push_back(0);
+            for(unsigned int k=0;k<b.size();k++){
+                //c[i][j]+=a[i][k]*b[k][j];
+                ans.back()+=a[i][k]*b[k][j];
+                }
+            }
+        }
+    sort(ans.begin(),ans.end());
+    for(int x:ans)
+        printf("%d\n",x);
     return 0;
     }
